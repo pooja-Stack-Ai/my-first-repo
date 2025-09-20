bde81e277fdc7a7e5ee4c66b8fda86d72e6cc49b
pooja-Stack-Ai <poojajha.2368@gmail.com>
2025-09-21 02:32:46 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,1 +1,6 @@
 echo 'print("Hello, GitHub!")' > hello.py
+ # My first Python program
+   name = input("What's your name? ")
+   age = input("How old are you? ")
+   print(f"Hello {name}! You are {age} years old.")
+   print("Welcome to GitHub!")
